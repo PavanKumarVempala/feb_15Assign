d084bebaee6b813c39df8f4aa8e7b5b3a7f2a90c
Pavan <vpavankumar1999@gmail.com>
2024-02-15 15:00:10 +0530

    commit final

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Enter the first number:");
+        string input1 = Console.ReadLine();
+
+        Console.WriteLine("Enter the second number:");
+        string input2 = Console.ReadLine();
+
+        if (double.TryParse(input1, out double number1) && double.TryParse(input2, out double number2))
+        {
+            double sum = number1 + number2;
+            Console.WriteLine($"The sum of {number1} and {number2} is: {sum}");
+        }
+        else
+        {
+            Console.WriteLine("Invalid input. Please enter valid numbers.");
+        }
+    }
+}
